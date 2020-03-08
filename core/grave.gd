fe6a7582801ba35df3a947a096314497762b5008
VictorSeven <vbuendiar@gmail.com>
2020-03-08 20:58:41 +0100

    Added new lines for graveyard

--- core/grave.gd
+++ core/grave.gd
@@ -2,7 +2,17 @@ extends Sprite
 
 
 var frases = ["I was caught by a ghost.\r\n Now I sleep here.",
-				"Guess I should have run faster..."]
+				"Guess I should have run faster...",
+				"Be careful with ghosts. \r\nDon't let them get you.",
+				"Get the key, get the key, \r\nget the key. Get out.",
+				"Did you read the graveyard's tombs?\r\nI should have read them",
+				"The tower is very big.\r\nIs the key there?",
+				"There's even water inside the cave!\r\nThe key could be there...",
+				"The pumpkins are very beautiful...\r\nCould the key be hidden with them?",
+				"Oh, the wharf... There's no more ships in this region,\r\nbut maybe the key is there.",
+				"Get out of the forest\r\nYou'll finde nothing here",
+				"Oh, I hate those ghosts!\r\nThey actually took me...",
+				"Colors, lights... they follow you...\r\nDon't let the colored ghost touch you!"]
 
 #Select a random text for the grave
 func _ready():
